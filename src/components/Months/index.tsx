00d3466b83317f84f7d9fa6f8196d2da8d404491
Neto Sousa <netosouzacp@gmail.com>
2023-09-20 16:04:27 -0300

    feat: create months component

---
+++ src/components/Months/index.tsx
@@ -0,0 +1,12 @@
+import { MonthBtn } from '../base/MonthBtn'
+
+export function Months() {
+    return (
+        <div className="flex">
+            <MonthBtn monthNumber={-3} />
+            <MonthBtn monthNumber={-2} />
+            <MonthBtn monthNumber={-1} />
+            <MonthBtn monthNumber={0} />
+        </div>
+    )
+}
