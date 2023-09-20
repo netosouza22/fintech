7b1a67d668198cca328d679aca2809278fa11e4f
Neto Sousa <netosouzacp@gmail.com>
2023-09-20 16:06:24 -0300

    refactor: change the page structure compoents

--- src/pages/Resumo/index.tsx
+++ src/pages/Resumo/index.tsx
@@ -1,15 +1,11 @@
-import { DateRange } from "../../components/DateRange";
-import { Header } from "../../components/Header";
-import { Sidenav } from "../../components/Sidenav";
+import { Header } from '../../components/Header'
+import { Sidenav } from '../../components/Sidenav'
 
 export default function Resumo() {
-  return(
-    <div className="container">
-      <Header />
-      <Sidenav />
-      <main>
-        <DateRange />
-      </main>
-    </div>
-  )
+    return (
+        <div className="container">
+            <Sidenav />
+            <Header />
+        </div>
+    )
 }
