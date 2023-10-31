64f88d90dd154e984e850556c7fc1585c8848ea3
Neto Sousa <netosouzacp@gmail.com>
2023-10-31 17:54:45 -0300

    feat: add links to nav bar

--- src/components/Sidenav/index.tsx
+++ src/components/Sidenav/index.tsx
@@ -1,5 +1,59 @@
+import { NavLink } from 'react-router-dom'
+import FintechSVG from '../../assets/FintechSVG'
+import configuracoes from '../../assets/icons/configuracoes.svg'
+import contato from '../../assets/icons/contato.svg'
+import resumo from '../../assets/icons/resumo.svg'
+import sair from '../../assets/icons/sair.svg'
+import vendas from '../../assets/icons/vendas.svg'
+import webhooks from '../../assets/icons/webhooks.svg'
+
 export function Sidenav() {
-  return(
-    <nav>Sidenav</nav>
-  )
+    return (
+        <nav className="sidenav box bg-3">
+            <FintechSVG title="fintech logo" />
+            <ul>
+                <li>
+                    <span>
+                        <img src={resumo} alt=""></img>
+                    </span>
+                    <NavLink to="/">Resumo</NavLink>
+                </li>
+
+                <li>
+                    <span>
+                        <img src={vendas} alt=""></img>
+                    </span>
+                    <NavLink to="/vendas">Vendas</NavLink>
+                </li>
+
+                <li>
+                    <span>
+                        <img src={webhooks} alt=""></img>
+                    </span>
+                    <a href="">Webhooks</a>
+                </li>
+
+                <li>
+                    <span>
+                        <img src={configuracoes} alt=""></img>
+                    </span>
+                    <a href="">Configurações</a>
+                </li>
+
+                <li>
+                    <span>
+                        <img src={contato} alt=""></img>
+                    </span>
+                    <a href="">Contato</a>
+                </li>
+
+                <li>
+                    <span>
+                        <img src={sair} alt=""></img>
+                    </span>
+                    <a href="">Sair</a>
+                </li>
+            </ul>
+        </nav>
+    )
 }
