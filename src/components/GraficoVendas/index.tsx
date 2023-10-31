abbf18092013b497a558d76f7fc1d760ba19a71f
Neto Sousa <netosouzacp@gmail.com>
2023-10-31 17:52:33 -0300

    feat: create grafico vendas components

---
+++ src/components/GraficoVendas/index.tsx
@@ -0,0 +1,9 @@
+import { IVenda } from '../../contexts/DataContext'
+
+interface GraficoVendasProps {
+    data: IVenda[]
+}
+
+export function GraficoVendas({ data }: GraficoVendasProps) {
+    return <div>Graficos Vendas</div>
+}
