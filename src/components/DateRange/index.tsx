3bf49e5668d4466e5f5f36c92cb5292139fb2999
Neto Sousa <netosouzacp@gmail.com>
2023-11-23 23:44:32 -0300

    feat: add class box to dateRange

--- src/components/DateRange/index.tsx
+++ src/components/DateRange/index.tsx
@@ -1,15 +1,22 @@
-import { useData } from "../../contexts/DataContext";
-import { DateInput } from "../base/DateInput";
+import { useData } from '../../contexts/DataContext'
+import { DateInput } from '../base/DateInput'
 
 export const DateRange = () => {
-const {inicio, setInicio, final, setFinal} = useData()
+    const { inicio, setInicio, final, setFinal } = useData()
 
-  return(
-<form className="flex" onSubmit={e => e.preventDefault()}>
-    <DateInput  label="Inicio" value={inicio} onChange={(e) => setInicio(e.target.value)} />
-    {inicio}
-    <DateInput label="final" value={final} onChange={(e) => setFinal(e.target.value)}  />
-</form>
+    return (
+        <form className="flex box" onSubmit={(e) => e.preventDefault()}>
+            <DateInput
+                label="Inicio"
+                value={inicio}
+                onChange={(e) => setInicio(e.target.value)}
+            />
+            {inicio}
+            <DateInput
+                label="final"
+                value={final}
+                onChange={(e) => setFinal(e.target.value)}
+            />
+        </form>
     )
-
 }
