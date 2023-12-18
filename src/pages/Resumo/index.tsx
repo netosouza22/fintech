b2c4aa6f619a30e68d4721ad29f1693b4769e4b0
Neto Sousa <netosouzacp@gmail.com>
2023-12-17 21:22:37 -0300

    refactor: add btnMonths

--- src/pages/Resumo/index.tsx
+++ src/pages/Resumo/index.tsx
@@ -10,55 +10,60 @@ export default function Resumo() {
     return null
   }
   return (
-    <div className="container">
-      <Months />
-      {loading ? (
-        <Loading />
-      ) : (
-        <section>
-          <div className="resumo flex mb">
-            <div className="box">
-              <h2>Vendas</h2>
-              <span>
-                {data
-                  .filter((item) => item.status !== 'falha')
-                  .reduce((acc, item) => acc + item.preco, 0)
-                  .toLocaleString('pt-br', {
-                    style: 'currency',
-                    currency: 'BRL',
-                  })}
-              </span>
+    <div>
+      <div className="mb">
+        <Months />
+      </div>
+
+      <div>
+        {loading ? (
+          <Loading />
+        ) : (
+          <section>
+            <div className="resumo flex mb">
+              <div className="box">
+                <h2>Vendas</h2>
+                <span>
+                  {data
+                    .filter((item) => item.status !== 'falha')
+                    .reduce((acc, item) => acc + item.preco, 0)
+                    .toLocaleString('pt-br', {
+                      style: 'currency',
+                      currency: 'BRL',
+                    })}
+                </span>
+              </div>
+              <div className="box">
+                <h2>Recebidos</h2>
+                <span>
+                  {data
+                    .filter((item) => item.status === 'pago')
+                    .reduce((acc, item) => acc + item.preco, 0)
+                    .toLocaleString('pt-br', {
+                      style: 'currency',
+                      currency: 'BRL',
+                    })}
+                </span>
+              </div>
+              <div className="box">
+                <h2>Processando</h2>
+                <span>
+                  {data
+                    .filter((item) => item.status === 'processando')
+                    .reduce((acc, item) => acc + item.preco, 0)
+                    .toLocaleString('pt-br', {
+                      style: 'currency',
+                      currency: 'BRL',
+                    })}
+                </span>
+              </div>
             </div>
-            <div className="box">
-              <h2>Recebidos</h2>
-              <span>
-                {data
-                  .filter((item) => item.status === 'pago')
-                  .reduce((acc, item) => acc + item.preco, 0)
-                  .toLocaleString('pt-br', {
-                    style: 'currency',
-                    currency: 'BRL',
-                  })}
-              </span>
-            </div>
-            <div className="box">
-              <h2>Processando</h2>
-              <span>
-                {data
-                  .filter((item) => item.status === 'processando')
-                  .reduce((acc, item) => acc + item.preco, 0)
-                  .toLocaleString('pt-br', {
-                    style: 'currency',
-                    currency: 'BRL',
-                  })}
-              </span>
-            </div>
-          </div>
-          <section className="box mb">
-            <GraficoVendas data={data} />
+            <section className="box mb">
+              <GraficoVendas data={data} />
+            </section>
           </section>
-        </section>
-      )}
+        )}
+      </div>
     </div>
   )
 }
--- src/pages/Vendas/index.tsx
+++ src/pages/Vendas/index.tsx
@@ -1,19 +1,25 @@
+import { Months } from '../../components/Months'
 import { VendaItem } from '../../components/VendaItem'
 import { useData } from '../../contexts/DataContext'
 
 export default function Vendas() {
-    const { data } = useData()
+  const { data } = useData()
 
-    if (data === null) return null
+  if (data === null) return null
 
-    return (
-        <ul>
-            {data.map((venda) => (
-                <li key={venda.id}>
-                    {' '}
-                    <VendaItem venda={venda} />
-                </li>
-            ))}
-        </ul>
-    )
+  return (
+    <>
+      <div className="mb">
+        <Months />
+      </div>
+      <ul>
+        {data.map((venda) => (
+          <li key={venda.id}>
+            {' '}
+            <VendaItem venda={venda} />
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
